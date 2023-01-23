37a6eeac9846dbdd7f1ad14ef76ad9bf1f0c3364
alinarozhkovska <119886599+alinarozhkovska@users.noreply.github.com>
2023-01-24 01:56:41 +0200

    third task

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def show_movies_hist(df_titles):
     plt.hist(movie_scores, bins = 40)
     plt.show()
 
-    print(f"The best movie {movies[movies['imdb_score'] == movies['imdb_score'].max()]['title']}")
+    print(f"The best movie:\n{movies[movies['imdb_score'] == movies['imdb_score'].max()]['title']}")
 
 def show_shows_hist(df_titles):
     shows = df_titles[df_titles["type"] == 'SHOW']
@@ -19,7 +19,7 @@ def show_shows_hist(df_titles):
     plt.hist(shows_scores, bins = 40)
     plt.show()
 
-    print(f"The best show {shows[shows['imdb_score'] == shows['imdb_score'].max()]['title']}")
+    print(f"The best show:\n{shows[shows['imdb_score'] == shows['imdb_score'].max()]['title']}")
 
 def run_first_task(df_titles):
     show_movies_hist(df_titles)
@@ -38,6 +38,36 @@ def shows_age_certification_pie(df_titles):
 def run_second_task(df_titles):
     shows_age_certification_pie(df_titles)
 
+
+def show_procents_good_movies(df_titles):
+    new_films = df_titles[df_titles['release_year'] >= 2000]
+
+    years = list(new_films['release_year'].unique())
+    years.sort()
+
+    good_movies_procents = []
+
+    for year in years:
+      movies_count = len(df_titles[df_titles['release_year'] == year])
+      movies_good_rate_count = len(df_titles[(df_titles['release_year'] == year) & (df_titles['imdb_score'] >= 8)])
+      procent_of_good_movies = movies_good_rate_count/(movies_count/100)
+      good_movies_procents.append(procent_of_good_movies)
+
+    plt.plot(years, good_movies_procents)
+    plt.show()
+
+    best_procent = max(good_movies_procents)
+
+    index_best_procents = good_movies_procents.index(best_procent)
+
+    print(f"Best year is {years[index_best_procents]}")
+
+def run_third_task(df_titles):
+    show_procents_good_movies(df_titles)
+
 run_first_task(df_titles)
 run_second_task(df_titles)
+run_third_task(df_titles)
+
+
 
