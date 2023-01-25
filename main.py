fc59926cdabc310c2b0df638408303abcf448887
Sofiia Dubova <sdubova@kse.org.ua>
2023-01-25 20:12:23 +0200

    fifth task

--- main.py
+++ main.py
@@ -84,7 +84,48 @@ def run_fourth_task(df_titles, df_credits):
     show_actors_good_movies(df_titles, df_credits)
 
 
+def get_list_genres(g_movies, type):
+    type_movie = g_movies[g_movies['type'].isin([type])]
+
+    list_genres_movies = []
+    for row in type_movie['genres'].tolist():
+        list_genres_movies += row[1:-1].split(', ')
+    list_genres_movies.sort()
+    result = pd.DataFrame(list_genres_movies, columns=['genres'])
+
+    return result
+
+
+def show_count_genre(df_titles):
+    df_titles.sort_values('imdb_score', inplace=True, ascending=False)
+    good_movies = df_titles[:1000]
+
+    get_shows = get_list_genres(good_movies, 'SHOW')
+    get_movies = get_list_genres(good_movies, 'MOVIE')
+
+    x1 = np.arange(0, 20) + 0.2
+    x2 = np.arange(0, 20) - 0.2
+    y1 = np.array(get_shows['genres'].value_counts(sort=False).tolist())
+    y2 = np.array(get_movies['genres'].value_counts(sort=False).tolist())
+
+    fig, ax = plt.subplots()
+
+    ax.bar(x1, y1, width=0.4)
+    ax.bar(x2, y2, width=0.4)
+
+    fig.set_figwidth(10)
+    fig.set_figheight(8)
+    plt.xticks(x1 - 0.2, get_movies['genres'].unique(), rotation=90)
+
+    plt.show()
+
+
+def run_fifth_task(df_titles):
+    show_count_genre(df_titles)
+
+
 run_first_task(df_titles)
 run_second_task(df_titles)
 run_third_task(df_titles)
 run_fourth_task(df_titles, df_credits)
+run_fifth_task(df_titles)
